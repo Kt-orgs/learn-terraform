489d22af5ed705abe7e9f9f27793534a8a236636
krityagi <126631875+krityagi@users.noreply.github.com>
2025-08-01 11:59:13 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,5 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
-}
 
