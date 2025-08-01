3ab1f46a1b97fb9448dae6b9e347e2559d79dbda
krityagi <126631875+krityagi@users.noreply.github.com>
2025-08-01 11:59:02 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,16 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
 
 variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+  description = "GCP region"
+  type        = string
+  default     = "europe-west2"
 }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "europe-west2-a"
 }
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
-}
-
