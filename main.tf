4ea051a723d11d0653496387a0a37f4258b93f3a
krityagi <ktyagi0602@gmail.com>
2025-08-07 15:37:48 +0100

    destroy

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
+# provider "google" {
+#   project = var.project_id
+#   region  = var.region
+#   zone    = var.zone
+# }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "new-vm"
-  machine_type = "e2-micro"
-  zone         = var.zone
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "new-vm"
+#   machine_type = "e2-micro"
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
 
-  network_interface {
-    network       = "default"
-    access_config {}  # Allocates public IP
-  }
+#   network_interface {
+#     network       = "default"
+#     access_config {}  # Allocates public IP
+#   }
 
-  tags = ["terraform-demo"]
-}
+#   tags = ["terraform-demo"]
+# }
